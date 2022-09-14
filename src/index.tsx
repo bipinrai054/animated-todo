38b6b4e2bc1076bc5fe357525b49d0640cbe5fec
bipnrai054 <bipinrai054@gmail.com>
2022-09-14 14:26:40 +0545

    about screen added

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 
 import MainScreen from './screens/main'
+import AboutScreen from './screens/about-screen'
 
 const Drawer = createDrawerNavigator()
 
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <Drawer.Navigator initialRouteName="Main">
       <Drawer.Screen name="main" component={MainScreen}></Drawer.Screen>
+      <Drawer.Screen name="about" component={AboutScreen}></Drawer.Screen>
     </Drawer.Navigator>
   )
 }
